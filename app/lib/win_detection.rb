919d3509105ebb3081b6165ce550f33da197c1bb
hsearcy <houston.searcy@careerbuilder.com>
2018-01-17 18:35:25 -0500

    move gameplay logic to better places

---
+++ app/lib/win_detection.rb
@@ -0,0 +1,95 @@
+class WinDetection
+  def self.is_winner? (boardstatus, move_col, move_row, player)
+    return player if (  self.vertical_win(boardstatus, move_col, player) || 
+                        self.horizontal_win(boardstatus, move_col, move_row, player) ||
+                        self.left_top_diagonal_win(boardstatus, move_col, move_row, player) ||
+                        self.right_top_diagonal_win(boardstatus, move_col, move_row, player)
+                      )
+    
+    return 0;
+  end
+
+  def self.vertical_win(boardstatus, move_col, player)
+    return true if boardstatus[move_col].chunk{|disc| disc == player && disc }
+                                        .any? { |same_player, consecutive| same_player && consecutive.count > 3 }
+    false
+  end
+
+  def self.horizontal_win(boardstatus, move_col, move_row, player)
+    continue_left = true
+    continue_right = true
+    left_matches = 0
+    right_matches = 0
+    (1..3).each do |i| 
+      if continue_left
+        if move_col - i < 0 || boardstatus[move_col - i][move_row] != player
+          continue_left = false
+        else
+          left_matches += 1
+        end
+      end
+      if continue_right
+        if move_col + i > 6 || boardstatus[move_col + i][move_row] != player
+          continue_right = false
+        else
+          right_matches += 1
+        end
+      end
+      return true if (left_matches + right_matches) > 2
+      return false unless ( continue_left || continue_right )
+    end
+    return false
+  end
+
+  def self.left_top_diagonal_win(boardstatus, move_col, move_row, player)
+    continue_left = true
+    continue_right = true
+    left_matches = 0
+    right_matches = 0
+    (1..3).each do |i| 
+      if continue_left
+        if move_col - i < 0 || move_row + i > 5 || boardstatus[move_col - i][move_row + i] != player
+          continue_left = false
+        else
+          left_matches += 1
+        end
+      end
+      if continue_right
+        if move_col + i > 6 || move_row - i < 0 || boardstatus[move_col + i][move_row - i] != player
+          continue_right = false
+        else
+          right_matches += 1
+        end
+      end
+      return true if (left_matches + right_matches) > 2
+      return false unless ( continue_left || continue_right )
+    end
+    return false
+  end
+
+  def self.right_top_diagonal_win(boardstatus, move_col, move_row, player)
+    continue_left = true
+    continue_right = true
+    left_matches = 0
+    right_matches = 0
+    (1..3).each do |i| 
+      if continue_left
+        if move_col - i < 0 || move_row - i < 0 || boardstatus[move_col - i][move_row - i] != player
+          continue_left = false
+        else
+          left_matches += 1
+        end
+      end
+      if continue_right
+        if move_col + i > 6 || move_row + i > 5 || boardstatus[move_col + i][move_row + i] != player
+          continue_right = false
+        else
+          right_matches += 1
+        end
+      end
+      return true if (left_matches + right_matches) > 2
+      return false unless ( continue_left || continue_right )
+    end
+    return false
+  end
+end
--- app/models/game.rb
+++ app/models/game.rb
@@ -4,12 +4,22 @@ class Game < ApplicationRecord
   def self.setup
     game = self.new
     game.boardstatus = Array.new(7){ Array.new(6){0} }
-    game.mode = 1
     game.move = 1
     game.save
-    puts 'game'
-    puts game.id
-    puts game
     return game
   end
+
+  def self.updateGame(id, move_col)
+    game = self.find(id)
+    move_row = game.boardstatus[move_col].find_index { |disc| disc == 0 }
+
+    unless move_row.nil? 
+      game.boardstatus[move_col][move_row] = game.move
+      game.winner = WinDetection.is_winner?(game.boardstatus, move_col, move_row, game.move)
+      game.move == 1 ? game.move = 2 : game.move = 1
+      game.save
+    end
+    return game
+  end
+
 end
