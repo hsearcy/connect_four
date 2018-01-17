1d17baa89ea9392745b4870b05a9cdd3fa8fb4e4
hsearcy <houston.searcy@careerbuilder.com>
2018-01-17 15:35:40 -0500

    working on winner check logic

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -4,27 +4,38 @@ class GameController < ApplicationController
 
   def new
     game = Game.setup
-    puts 'controller'
-    puts game.to_json
     render json: game.to_json
   end
 
   def move
     game = Game.find(params[:id])
-    puts params[:moveCol]
-    puts game.to_json
-    game = validateMove(game, params[:moveCol])
+    game = updateGame(game, params[:moveCol])
     render json: game.to_json
   end
 
   private
-  def validateMove(game, move_col)
-    valid_disc = game.boardstatus[move_col].find_index { |disc| disc == 0 }
-    unless valid_disc.nil? 
-      game.boardstatus[move_col][valid_disc] = game.move
+
+  def updateGame(game, move_col)
+    move_row = game.boardstatus[move_col].find_index { |disc| disc == 0 }
+
+    unless move_row.nil? 
+      game.boardstatus[move_col][move_row] = game.move
+      game.winner = checkWinner(game.boardstatus, move_col, move_row, game.move)
       game.move == 1 ? game.move = 2 : game.move = 1
       game.save
     end
     return game
+  end
+
+  def checkWinner(boardstatus, move_col, move_row, player)
+    return player if checkVertical(boardstatus, move_col, move_row)
+    
+    return 0;
+  end
+
+  def checkVertical(boardstatus, move_col, player)
+    return true if boardstatus[move_col].chunk{|disc| disc == player && disc }
+                                        .any? { |same_player, consecutive| same_player && consecutive.count > 3 }
+    false
   end
 end
